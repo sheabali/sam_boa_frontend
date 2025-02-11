620746db9d1a9e40c0060e80b2baad34710e5313
Meraj Faizan <mdmaraj1500@gmail.com>
2025-02-11 10:28:30 +0600

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,31 +1,37 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        primary: "#7C45F9",
-        secondary: "#102F22",
-        success: "#102F22",
-        info: "#D9F4F9",
-        warning: "#FAE4D0",
-        danger: "#d7191c",
-      },
-      container: {
-        center: true,
-        padding: "1rem",
-        screens: {
-          default: "1440px",
-        },
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)',
+  			primary: '#7C45F9',
+  			secondary: '#102F22',
+  			success: '#102F22',
+  			info: '#D9F4F9',
+  			warning: '#FAE4D0',
+  			danger: '#d7191c'
+  		},
+  		container: {
+  			center: true,
+  			padding: '1rem',
+  			screens: {
+  				default: '1440px'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
